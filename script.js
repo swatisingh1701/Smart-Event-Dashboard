3921e2e3be2ec644a7aa71a394e3a0fb59658908
Swati Singh <swati98207@gmail.com>
2026-02-15 22:10:17 +0530

    adding transitions and basic dom structure of dashboard

---
+++ script.js
@@ -0,0 +1,11 @@
+let events = [];
+
+const eventForm = document.querySelector('#event'); 
+const eventTitle = document.querySelector('#event-title');
+const eventDate = document.querySelector('#event-date');
+const eventCategory = document.querySelector('#event-category');
+const eventDescription = document.querySelector('#event-description');
+const errorMessage = document.querySelector('#error-message');
+const eventsContainer = document.querySelector('#events-container');
+const clearBtn = document.querySelector('#clear-btn');
+const sampleBtn = document.querySelector('#sample-btn');
